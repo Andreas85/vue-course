1b846288702ed1396e980243efcc01bacebd9562
Andreas Larsson <andreas.l85@protonmail.com>
2022-07-31 17:44:24 +0200

    Assignment 6

---
+++ assignment-6/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="component-container">
+    <active-user :username="username" :age="age"></active-user>
+  </div>
+  <div class="component-container">
+    <user-data @username-input="updateUsername" @age-input="updateAge"></user-data>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      username: 'testuser',
+      age: 0
+    };
+  },
+  methods: {
+    updateUsername(username) {
+      this.username = username;
+    },
+    updateAge(age) {
+      this.age = parseInt(age);
+    }
+  }
+}
+
+</script>
+
+<style>
+
+.component-container {
+  width: 30%;
+  float: left;
+}
+
+</style>
---
+++ assignment-6/src/components/UserData.vue
@@ -0,0 +1,23 @@
+<template>
+  <label for="username">Username</label><input type="text" @input="$emit('username-input', username)" v-model="username"><br>
+  <label for="age">Age</label><input type="text" @input="$emit('age-input', age)" v-model="age"><br>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      username: '',
+      age: 0
+    };
+  },
+  emits: ['username-input', 'age-input'],
+}
+
+</script>
+
+
+<style>
+
+</style>
